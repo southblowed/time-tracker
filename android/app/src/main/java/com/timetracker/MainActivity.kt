047930c5795971830f951c355e3f08007147ecbf
southblowed <southblowed@users.noreply.github.com>
2026-05-25 15:38:39 +0800

    全新Android项目: Kotlin + API 36 + apksigner签名(v1+v2+v3)

---
+++ android/app/src/main/java/com/timetracker/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.timetracker.app
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.databaseEnabled = true
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            settings.builtInZoomControls = false
+            settings.displayZoomControls = false
+            settings.allowFileAccess = true
+            settings.cacheMode = WebSettings.LOAD_DEFAULT
+            settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+
+            webChromeClient = WebChromeClient()
+            webViewClient = WebViewClient()
+
+            loadUrl("file:///android_asset/index.html")
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
